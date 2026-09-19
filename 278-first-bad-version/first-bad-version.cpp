7ebb84d1c25df591295c8d9d45a3354c1078f899
Yasir Asfaque <yasirasfaque91@gmail.com>
2026-09-19 19:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (5.32%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+     int start=0;
+     int end=n;
+     int bad;
+     while(start<=end){
+        int mid=start+(end-start)/2;
+        if(isBadVersion(mid)){
+            bad=mid;
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+     }
+     return bad;
+    }
+};
