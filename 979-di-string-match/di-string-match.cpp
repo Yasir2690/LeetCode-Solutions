70ef5b510c57dbf635d38070d9b35b8cc45b1d5c
Yasir Asfaque <yasirasfaque91@gmail.com>
2026-09-24 22:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (32.61%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int> ans;
+        int low = 0, high = s.size();
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'I') {
+                ans.push_back(low);
+                low++;
+            } else {
+                ans.push_back(high);
+                high--;
+            }
+        }
+        ans.push_back(high);
+        return ans;
+    }
+};
